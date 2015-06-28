2ee3c10f2c29d8ca9725ae2a5417b6573b8db51d
Ahkari <gqz825584053@qq.com>
2015-06-28 20:04:08 +0800

    基本完成网页布局
    本版本测试外链字体

--- demo/js/index.js
+++ demo/js/index.js
@@ -18,7 +18,7 @@ $(function(){
 		}
 		var eventJq = $.event.fix(event || window.event); 
 		var $target = $(eventJq.target) ;
-		if ($target.hasClass('title') && delta=== -1){
+		if (($target.hasClass('title')||$target.parents('section').hasClass('title') )&& delta=== -1){
 			$('.title').css({'height' : '50px','background-color':'#1c1c1c'});
 			$('.model').css({'background-color':'#272727'});
 
@@ -27,7 +27,6 @@ $(function(){
 			$('.viewBtn').css({'opacity':'1'});
 
 			infoHide();
-
 		}else if($target.hasClass('title') && delta === 1){
 			$('.title').css({'height':titleHeight ,'background-color':'#272727'});
 			$('.model').css({'background-color':'#1c1c1c'});
@@ -37,8 +36,61 @@ $(function(){
 			setTimeout(function(){$('.viewBtn').css({'visibility':'hidden'})},300);
 
 			infoShow();
+		}else if($target.hasClass('chooseMain') || $target.parents('div').hasClass('chooseMain')){
+			if (delta === -1){
+				$('.main').css({'left':-680});
+				$('.tab').css({'background-color':'#383838'});
+				$('.choose').removeClass('tab-active');
+				$('.choose').css({'background-color':'rgba(255, 255, 255, 0.08)'})
+				$('.config').addClass('tab-active');
+				$('.config').css({'background-color':'rgb(45, 45, 45)'});
+			}
+		}else if($target.hasClass('configMain') || $target.parents('div').hasClass('configMain')){
+			if (delta === -1){
+				$('.main').css({'left':-1570});
+				$('.tab').css({'background-color':'#222222'});
+				$('.config').removeClass('tab-active');
+				$('.config').css({'background-color':'rgba(255, 255, 255, 0.08)'})
+				$('.about').addClass('tab-active');
+				$('.about').css({'background-color':'rgb(68, 68, 68)'})
+			}else if(delta === 1){
+				$('.main').css({'left':210});
+				$('.tab').css({'background-color':'#2f2f2f'});
+				$('.config').removeClass('tab-active');
+				$('.config').css({'background-color':'rgba(255, 255, 255, 0.08)'})
+				$('.choose').addClass('tab-active');
+				$('.choose').css({'background-color':'rgb(39, 39, 39)'});
+			}
+		}else if($target.hasClass('aboutMain') || $target.parents('div').hasClass('aboutMain')){
+			if (delta === -1){
+
+			}else if(delta === 1){
+				$('.main').css({'left':-680});
+				$('.tab').css({'background-color':'#383838'});
+				$('.about').removeClass('tab-active');
+				$('.about').css({'background-color':'rgba(255, 255, 255, 0.08)'})
+				$('.config').addClass('tab-active');
+				$('.config').css({'background-color':'rgb(45, 45, 45)'})
+			}
 		}
+
+
+
 	});
+
+	
+	$('.chooseMain .transModel').click(function(e){
+		var eventJq = $.event.fix(e || window.event); 
+		$target = $(eventJq.target);
+		$target = $target.parent();
+		if ($target.hasClass('transModel')){
+			var X_offset = $target.offset().left - $('.transModel.adv-sr').offset().left;
+			var Y_offset = $target.offset().top - $('.transModel.adv-sr').offset().top;
+			$('.transModel2').css({'opacity':1,'left': 80 + X_offset,'top': 48 + Y_offset });
+			$('.transModel p').removeClass('list-active');
+			$target.find('p').addClass('list-active');
+		}
+	})
 
 
 })
