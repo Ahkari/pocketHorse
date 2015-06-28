f6f7d1c8a69bfdf94031163499734fee6e614bc6
Ahkari <gqz825584053@qq.com>
2015-06-28 22:04:09 +0800

    增加点击切换动画效果, 增加全局定时器清除, 增加id容错判断

--- js/index.js
+++ js/index.js
@@ -89,8 +89,75 @@ $(function(){
 			$('.transModel2').css({'opacity':1,'left': 80 + X_offset,'top': 48 + Y_offset });
 			$('.transModel p').removeClass('list-active');
 			$target.find('p').addClass('list-active');
+
+			var model = $target.data('model') ;
+			//记得清除之前的定时器!
+			window.clearInterval(defaultTimer);	
+			
+			$('.common-carouselImage').empty();
+			//视模式绑定
+			var domStr = '<a class="carousLeftArr pa"><i class="iconB iconLeft"></i></a><div class="imageArea"><img src="./image/IU.jpg"></div><a class="carousRightArr pa"><i class="iconB iconRight"></i></a>';
+			setTimeout(function(){
+				$('.common-carouselImage').append(domStr);
+				$('.common-carouselImage').pocketHorse({
+					dotControl : true ,
+					blockControl : true ,
+					isControlerHide : true ,
+					stayTime : 3000 ,
+					transTime : 1500 , 
+					transDirection : 'right' , //left,right,up,down//mult
+					transFunction : model , //模式选择
+					//普通模式: slide滑动，push推开，transparency透明
+					//高级模式: adv-scale 高级-缩放, adv-scaleRotate 高级-旋转缩放, adv-fragmentFly 高级-碎片飞行
+					transEasying : 'easeOutExpo' //动画曲线
+					//普通模式是swing,linear两种,其余的可外接jquery.easing插件,easeOutExpo
+					//高级模式支持css3的transition-timing-function属性,ease
+				}
+				,
+				[
+					'./image/IU.jpg',
+					'./image/35912162.jpg',
+					'./image/189.jpg',
+					'./image/saber.jpg',
+					'./image/892.jpg',
+					'./image/umberalla.jpg',
+					'./image/Wallpaper_1236.jpg'
+				]
+				);
+			},1000);
+
+			
+
 		}
-	})
+	});
+
+	//选项卡切换
+	$('.tab>ul>li').click(function(e){
+		var eventJq = $.event.fix(e || window.event); 
+		$target = $(eventJq.target);
+		if ($target.hasClass('choose')){
+			$('.main').css({'left':210});
+			$('.tab').css({'background-color':'#2f2f2f'});
+			$('.config').removeClass('tab-active');
+			$('.config').css({'background-color':'rgba(255, 255, 255, 0.08)'})
+			$('.choose').addClass('tab-active');
+			$('.choose').css({'background-color':'rgb(39, 39, 39)'});
+		}else if($target.hasClass('config')){
+			$('.main').css({'left':-680});
+			$('.tab').css({'background-color':'#383838'});
+			$('.choose').removeClass('tab-active');
+			$('.choose').css({'background-color':'rgba(255, 255, 255, 0.08)'})
+			$('.config').addClass('tab-active');
+			$('.config').css({'background-color':'rgb(45, 45, 45)'});
+		}else if($target.hasClass('about')){
+			$('.main').css({'left':-1570});
+			$('.tab').css({'background-color':'#222222'});
+			$('.config').removeClass('tab-active');
+			$('.config').css({'background-color':'rgba(255, 255, 255, 0.08)'})
+			$('.about').addClass('tab-active');
+			$('.about').css({'background-color':'rgb(68, 68, 68)'})
+		}
+	});
 
 
 })
